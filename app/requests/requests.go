6212c058d1d8807b7192b5bf4a037f29b4b64d72
fanzhuo <fans068@163.com>
2022-03-23 04:39:15 +0800

    bug fix shouldBindJson

--- app/requests/requests.go
+++ app/requests/requests.go
@@ -2,9 +2,10 @@
 package requests
 
 import (
+	"gohub/pkg/response"
+
 	"github.com/gin-gonic/gin"
 	"github.com/thedevsaddam/govalidator"
-	"gohub/pkg/response"
 )
 
 // ValidatorFunc 验证函数类型
@@ -14,10 +15,10 @@ type ValidatorFunc func(interface{}, *gin.Context) map[string][]string
 //        if ok := requests.Validate(c, &requests.UserSaveRequest{}, requests.UserSave); !ok {
 //            return
 //        }
-
 func Validate(c *gin.Context, obj interface{}, handler ValidatorFunc) bool {
+
 	// 1. 解析请求，支持 JSON 数据、表单请求和 URL Query
-	if err := c.ShouldBindJSON(obj); err != nil {
+	if err := c.ShouldBind(obj); err != nil {
 		response.BadRequest(c, err, "请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。")
 		return false
 	}
@@ -35,7 +36,6 @@ func Validate(c *gin.Context, obj interface{}, handler ValidatorFunc) bool {
 }
 
 func validate(data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
-
 	// 配置选项
 	opts := govalidator.Options{
 		Data:          data,
@@ -43,7 +43,17 @@ func validate(data interface{}, rules govalidator.MapData, messages govalidator.
 		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
 		Messages:      messages,
 	}
-
-	//开始验证
+	// 开始验证
 	return govalidator.New(opts).ValidateStruct()
+}
+
+func validateFile(c *gin.Context, data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
+	opts := govalidator.Options{
+		Request:       c.Request,
+		Rules:         rules,
+		Messages:      messages,
+		TagIdentifier: "valid",
+	}
+	// 调用 govalidator 的 Validate 方法来验证文件
+	return govalidator.New(opts).Validate()
 }

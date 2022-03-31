17b8eee4a7649e923416cbc53cb60392ddefbf0d
fanzhuo <fans068@163.com>
2022-04-01 03:51:52 +0800

    授权策略

---
+++ app/polices/topic_policy.go
@@ -0,0 +1,12 @@
+// Package polices 用户授权
+package polices
+
+import (
+	"github.com/gin-gonic/gin"
+	"gohub/app/models/topic"
+	"gohub/pkg/auth"
+)
+
+func CanModifyTopic(c *gin.Context, _topic topic.Topic) bool {
+	return auth.CurrentUID(c) == _topic.UserID
+}
